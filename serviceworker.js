bf5a586d55c0f5dc54230e5fcfc6f81be7b6ad0c
Ratheesh James <ratheeshjames@hotmail.com>
2021-05-02 22:41:54 +0530

    sw

--- serviceworker.js
+++ serviceworker.js
@@ -16,11 +16,11 @@ importScripts(
   });
   const cacheName = "vaccinecache";
   const staticAssets = [    
-    "/index.html",
-    "/js/vaccinetrack.js",
-    "/js/jquery-3.6.0.min.js", 
-    "/audio/notification.mp3",  
-    "/audio/notification.ogg", 
+    "/vaccine-track/index.html",
+    "/vaccine-track/js/vaccinetrack.js",
+    "/vaccine-track/js/jquery-3.6.0.min.js", 
+    "/vaccine-track/audio/notification.mp3",  
+    "/vaccine-track/audio/notification.ogg", 
   ]
   /*
     Enable precaching
