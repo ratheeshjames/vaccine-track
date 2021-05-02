7b11032f93afb976253d9baef092b56edaa114cf
Ratheesh James <ratheeshjames@hotmail.com>
2021-05-02 20:14:02 +0530

    changes

--- serviceworker.js
+++ serviceworker.js
@@ -1,24 +1,27 @@
-const staticDevCoffee = "dev-coffee-site-v1"
-const assets = [
-  "/",
-  "/index.html",
-  "/js/vaccinetrack.js",
-  "/js/jquery-3.6.0.min.js",
-
-]
-
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
-
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
-  })
+// Import Workbox (https://developers.google.com/web/tools/workbox/)
+importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"
+  );
+  
+  /*
+    Precache Manifest
+    Change revision as soon as file content changed
+  */
+  self.addEventListener("install", async (event) => {
+    console.log("install event");
+  });
+  
+  self.addEventListener("fetch", async (event) => {
+    console.log("fetch event");
+  });
+  const cacheName = "vaccinecache";
+  const staticAssets = [    
+    "index.html",
+    "js/vaccinetrack.js",
+    "js/jquery-3.6.0.min.js",    
+  ]
+  /*
+    Enable precaching
+    It is better to comment next line during development
+  */
+  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
