11da351821cd26123e51b8a32700752e84741338
Ratheesh James <ratheeshjames@hotmail.com>
2021-05-02 20:18:21 +0530

    dod

--- js/vaccinetrack.js
+++ js/vaccinetrack.js
@@ -2,7 +2,7 @@
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/vaccine-track/service-worker.js")
+    .register("vaccine-track/service-worker.js")
     .then(function () {
       console.log("Service Worker Registered");
     });
